224e91a7308e97f48c0a37214e2528cb0ab4afea
Farhad Hajnoruzi <farhad.hajnoruzi@gmail.com>
2022-03-19 13:15:43 +0100

    Db Initial population

--- WashIt.API/Data/DbPrep.cs
+++ WashIt.API/Data/DbPrep.cs
@@ -1,7 +1,41 @@
+using WashIt.API.Models;
+
 namespace WashIt.API.Data
 {
     public static class DbPrep
     {
+        public static void PreparePopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
 
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Devices.Any())
+            {
+                Console.WriteLine(" --> Populating Database...");
+
+                context.Devices.AddRange(
+                    new Machine() { Name = "Mahchine 01" },
+                    new Machine() { Name = "Mahchine 02" },
+                    new Machine() { Name = "Mahchine 03" },
+                    new Machine() { Name = "Mahchine 04" },
+                    new Machine() { Name = "Mahchine 05" },
+                    new Machine() { Name = "Mahchine 06" },
+                    new Machine() { Name = "Mahchine 07" },
+                    new Machine() { Name = "Mahchine 08" },
+                    new Machine() { Name = "Mahchine 09" },
+                    new Machine() { Name = "Mahchine 10" },
+                    new Machine() { Name = "Mahchine 11" },
+                    new Machine() { Name = "Mahchine 12" }
+                );
+                context.SaveChanges();
+            }
+            else
+                Console.WriteLine(" --> We already have data");
+        }
     }
 }
--- WashIt.API/Program.cs
+++ WashIt.API/Program.cs
@@ -30,4 +30,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+DbPrep.PreparePopulation(app);
+
 app.Run();
