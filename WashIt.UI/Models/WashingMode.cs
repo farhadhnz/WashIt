3616104122e9666deafd39ecbc7547e28e440fa0
Farhad Hajnoruzi <farhad.hajnoruzi@gmail.com>
2022-03-23 00:49:39 +0100

    Adding booking razor component

---
+++ WashIt.UI/Models/WashingMode.cs
@@ -0,0 +1,9 @@
+namespace WashIt.UI.Models
+{
+    public class WashingMode
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Temperature { get; set; }
+    }
+}
